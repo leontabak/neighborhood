4186b5cce96355fa3af0b264d8d1d3b1ca7d1cdd
Leon Tabak <l.tabak@ieee.org>
2015-09-22 15:38:53 -0500

    Add comments to code.

--- js/neighborhood.js
+++ js/neighborhood.js
@@ -10,9 +10,14 @@
 // Solon, IA 41.807, -91.494
 // West Branch, IA 41.671, -91.346
 
+// Define a function that will create an object
+// that describes Iowans who have contributed to
+// the development of science and technology.
 var model = function() {
     var that = {};
 
+    // Define a function that will create an object
+    // that describes a person (first, middle, and last name).
     var makePerson = function( firstName, middleName, lastName ) {
 	var result = {};
 
@@ -24,6 +29,9 @@ var model = function() {
 	return result;
     }; // makePerson()
 
+    // Define a function that will create an object
+    // that describes a person together with that person's
+    // date of birth and place of birth.
     var makePlace = function( latitude, longitude, 
             firstName, middleName, lastName, 
             city, state, birthday ) {
@@ -37,12 +45,15 @@ var model = function() {
         result.state = state;
 	result.birthday = birthday;
 
-        result.choose = function() { console.log( "look at this face" ); };
+        result.choose = function() { console.log( "look at this face: " + result.person.fullName ); };
 
         return result;
     }; // makePlace()
 
 
+    // Define an array of objects, each of which describes a
+    // person together with that person's date of birth and
+    // place of birth.
     var places = [];
     places.push( makePlace( 42.534, -92.445,
 			"Marc", "", "Andreessen", "Cedar Falls", "Iowa", new Date( 1971, 6, 9 ) ));
@@ -72,10 +83,13 @@ var model = function() {
     return that;
 }; // model()
 
-
+// Define a function that creates an object that
+// contains several methods that can be used to
+// access data that the model contains.
 var viewModel = function( m ) {
     var that = {};
 
+    // Define a function that compares two dates (year, month, day).
     that.compareDates = function( a, b ) {
         var aYear = a.getFullYear();
         var aMonth = a.getMonth();
@@ -112,6 +126,9 @@ var viewModel = function( m ) {
         } // else
     } // compareDates()
 
+    // Define a function that creates a string
+    // that represents a date with the full name
+    // of the month.
     that.dateToString = function( date ) {
 
         var monthName = "";
@@ -134,14 +151,23 @@ var viewModel = function( m ) {
         return result;
     } // dateToString()
 
+    // Define an accessor method that returns
+    // the number of place (person, date of birth, place of birth)
+    // objects in the model.
     that .getNumberOfPlaces = function() {
         return m.places.length;
     };
 
+    // Define an accessor method that returns a
+    // particular place (person, date of birth, and place of birth)
+    // that is in the model.
     that.getPlace = function( index ) {
         return m.places[index];
     };
 
+    // Define a method that computes the bounds and center of
+    // of region that contains all of the places that are
+    // described in the model.
     that.getExtremaAndMeans = function() {
         var result = {};
 
@@ -180,7 +206,7 @@ var viewModel = function( m ) {
         result.meanLatitude = meanLatitude;
         result.meanLongitude = meanLongitude;
 
-        result.minimumLatitude = meanLatitude;
+        result.minimumLatitude = minimumLatitude;
         result.maximumLatitude = maximumLatitude;
         result.minimumLongitude = minimumLongitude;
         result.maximumLongitude = maximumLongitude;
@@ -191,45 +217,128 @@ var viewModel = function( m ) {
     return that;
 }; // viewModel()
 
-
+// Define a function that will construct a map, markers,
+// and information windows.
 var view = function( vm ) {
     var that = {};
 
+    // Place the initial (default) range of years
+    // in the search bar.
     var loYear = 1846; // year that Iowa became a state
     var hiYear = (new Date()).getFullYear();
 
     $("#loYear").attr("value", loYear);
     $("#hiYear").attr("value", hiYear );
 
+
+    // Define a function that will attempt to find on Wikipedia the names
+    // of the colleges and universities that a person attended and append
+    // that data to the text in the information windows that is linked to
+    // the marker on the map at the place of that person's birth.
+    var getSchoolsAttended = function( firstName, lastName, informationWindow ) {
+
+        var parse = function( dataStructure ) {
+            var result = "";
+
+            // Find all of the text that lies between the
+            // word "alma_mater" and the character "|".
+            var str = JSON.stringify( dataStructure.query.pages );
+            var i = str.indexOf( "alma_mater" );
+            var j = str.indexOf( "|", i + 9 );
+            var almaMater = str.slice( i, j );
+
+            // Find names of schools enclosed in paired
+            // square brackets. For example, [[Worcester Polytechnic Institute]].
+            // Concatenate the names of the schools, placing an HTML <br> tag
+            // before each name.
+            var re = /\[\[[a-zA-Z ]*\]\]/g;
+            var schools;
+            while( schools = re.exec( almaMater ) ) {
+               var oneSchool = schools[0];
+               oneSchool = oneSchool.replace( "[[", "" );
+               oneSchool = oneSchool.replace( "]]", "" );
+               result = result + "<br>" + oneSchool;
+            } // while
+
+	    informationWindow.setContent( informationWindow.getContent() + result );
+        }; // parse()
+
+        // Specify the kind of access to the on-line service and
+        // the function that will do something with the data that
+        // is received.
+        var ajaxSettings = {
+            dataType: "jsonp",
+            crossDomain: true,
+            success: parse
+        }; // ajaxSettings
+
+        // Construct a URL that contains a query.
+        // This query searches for a page about a person
+        // that it identifies by first and last name.
+        var wikipediaURL = "http://en.wikipedia.org/w/api.php?action=query&format=json" +
+          "&prop=revisions&rvprop=content&titles=" + firstName + "%20" + lastName;
+
+        // Use a JQuery function to read data from an on-line service.
+        $.ajax( wikipediaURL, ajaxSettings );
+    }; // getSchoolsAttended()
+
+    // Define a function that can be used to create an information
+    // window that can be attached to a marker that is placed on a map.
     var makeInformationWindow = function( place ) {
         var today = new Date();
         var birthday = place.birthday;
+
+        // Just for fun, the model includes one (fictional)
+        // person whose date of birth is in the future.
+        // This makes it necessary to choose between two
+        // verb phrases: "was born in" or "will be born in".
         var verb = " was born in ";
 
         if( vm.compareDates( birthday, today ) > 0 ) {
             verb = " will be born in ";
         } // if
 
+        // The information window will contain the person's name,
+        // the city and state in which the person was born, and
+        // the date of the person's birth.
         var content = place.person.fullName + verb + place.city + ", " +
             place.state + " on " + vm.dateToString( place.birthday );
 
         return new google.maps.InfoWindow({content: content});
      }; // makeInformationWindow()
 
+     // Make all an array. 
+     // Make an information window for each person in the model.
+     // Read on Wikipedia the names of schools that person attended
+     // and append that list of schools to the text that is already
+     // in the information window.
+     // Store the information windows in the array.
      var informationWindows = [];
 
      for( var i = 0; i < vm.getNumberOfPlaces(); i++ ) {
+         var place = vm.getPlace(i);
+         var firstName = place.person.firstName;
+         var lastName = place.person.lastName;
          informationWindows[i] = makeInformationWindow( vm.getPlace(i) );
+         getSchoolsAttended( firstName, lastName, informationWindows[i] );
      } // for
 
+    // Compute the coordinates the center and eastern, western, northern,
+    // and southern boundaries of the region that contains all of the
+    // places of birth that are recorded in the model.
     var em = vm.getExtremaAndMeans();
 
+    // Describe the location of the center, the scale, and the type
+    // of the map that this program will draw.
     var mapSpecification = {
         center:new google.maps.LatLng(em.meanLatitude, em.meanLongitude),
         zoom:8,
         mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
+    }; // mapSpecification
 
+    // Adjust the boundaries to make the map a little bigger.
+    // Draw a region that is larger than the smallest region that
+    // encloses all of the places of birth.
     var margin = 0.5;
     var southwest = new google.maps.LatLng( em.minimumLatitude - margin, em.minimumLongitude - margin );
     var northeast = new google.maps.LatLng( em.maximumLatitude + margin, em.maximumLongitude + margin );
@@ -239,10 +348,12 @@ var view = function( vm ) {
     that.markers = [];
     that.neighborhoodMap = null;
 
-    function initializeMap() {
+    var initializeMap = function() {
 
+      // Create the map.
       that.neighborhoodMap=new google.maps.Map(document.getElementById("neighborhood"),mapSpecification);
 
+      // Create a marker for each object in the model and store in an array.
       for( var i = 0; i < vm.getNumberOfPlaces(); i++ ) {
           var marker = new google.maps.Marker({
     	      map: that.neighborhoodMap,
@@ -254,6 +365,9 @@ var view = function( vm ) {
           that.markers.push( marker );
       } // for
 
+      // Define a function that will tell a marker
+      // what to do when the mouse is clicked.
+      // (The marker will display its information window.)
       var addClickListener = function(i) {
           var marker = that.markers[i];
           var iw = informationWindows[i];
@@ -261,11 +375,17 @@ var view = function( vm ) {
           google.maps.event.addListener( marker, 'click', fun );
       }; // funMaker
 
-
+      // Associated the function that responds to mouse
+      // clicks with the marker that listens (waits) for 
+      // mouse clicks.
       for( var i = 0; i < that.markers.length; i++ ) {
           addClickListener(i);
       } // for
 
+      // Adjust the size of the map to allow for some
+      // space around all of the places of birth.
+      // (All places of birth will be visible, with space
+      // between the place and the map's boundary.)
       that.neighborhoodMap.fitBounds( bounds );
     } // initializeMap()
 
@@ -274,6 +394,7 @@ var view = function( vm ) {
     return that;
 }; // view()
 
+// Define the function that will build the model, viewModel, and view.
 var go = function() {
     var ourModel = model();
     var ourViewModel = viewModel( ourModel );
@@ -281,43 +402,54 @@ var go = function() {
 
     var myViewModel = function() {
         var that = this;
+        // Tell Knockout framework where to find the model
+        // that contains the data to be displayed on the page.
         that.places = ko.observableArray( ourModel.places );
 
+        // Define the function that will respond to input
+        // in the search field.
         that.filter = function( formElement) { 
+            // Read the years that were entered in the search box.
             var ly = $("#loYear").val();
             var hy = $("#hiYear").val();
 
-            console.log( ly + "---" + hy  ); 
-            console.log( "length of array = "  + ourModel.places.length );
+            //console.log( ly + "---" + hy  ); 
+            //console.log( "length of array = "  + ourModel.places.length );
+
+            // Remove (temporarily) all buttons from the model and page.
 	    that.places.removeAll();
+
 	    ourModel = model();
 	    var markers = ourView.markers;
 	    var neighborhoodMap = ourView.neighborhoodMap;
 
+            // Remove all markers from the map.
 	    for( var i = 0; i < markers.length; i++ ) {
 		markers[i].setMap(null);
 	    } // for
 
+            // Add all markers that identify the places of birth
+            // of people whose dates of birth lie within the
+            // specified range of years.
+            // Similarly, add all buttons that identify persons
+            // whose dates of birth lie within the specified range.
 	    for( var i = 0; i < ourModel.places.length; i++ ) {
                 var year = ourModel.places[i].birthday.getFullYear();
 		if( (ly < year) && (year < hy) ) {
 		    that.places.push( ourModel.places[i] );
 		    markers[i].setMap( neighborhoodMap );
-                    console.log( "add " + year );
+                    //console.log( "add " + year );
 		} // if
 		else {
-		    console.log( "do not add " + year );
+		    //console.log( "do not add " + year );
 		} // else
 	    } // for
         }; // filter()
 
-        that.choose = function() { 
-            console.log( "button pushed" ); 
-            $(".nameButton").removeClass( "nameButtonColor" );
-	    $(".nameButton").addClass( "selectedNameColor" ); 
-        };
     }; // myViewModel()
 
+    // Use Knockout framework to connect data in model
+    // with data displayed on HTML page.
     ko.applyBindings(new myViewModel() );
 }; // go()
 
